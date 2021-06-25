ecb92574dcec457de1a74a697b01900fd610a78e
Jizga <jizga93@gmail.com>
2021-06-25 20:53:20 +0000

    Try to get characters'details in Card component

--- src/front/js/component/Card.js
+++ src/front/js/component/Card.js
@@ -8,29 +8,14 @@ import StarWars from "../../img/StarWars.png";
 import "../../styles/card.scss";
 
 export function Card(props) {
-	console.log("props.favorite en Card -- ", props.favorite);
+	const { store, actions } = useContext(Context);
 
-	const { actions } = useContext(Context);
-
-	// const [characterDetails, setCharacterDetails] = useState(undefined);
-
+	//Se carga muchisimas veces (???????)
 	// useEffect(() => {
-	// 	getCharacterDetails(props.url);
+	// 	actions.getPeople(props.id);
 	// }, []);
 
-	// const getCharacterDetails = async url => {
-	// 	try {
-	// 		let response = await fetch(url);
-
-	// 		const data = await response.json();
-
-	// 		const data_detailsPeople = data.result;
-
-	// 		setCharacterDetails(data_detailsPeople);
-	// 	} catch (e) {
-	// 		console.error(`error from database -- ${e}`);
-	// 	}
-	// };
+	// console.log("store.detailsPeople desde Card : ", store.detailsPeople);
 
 	//Para añadir el personaje a la lista de favoritos
 	const favoriteDetails = () => {
@@ -47,17 +32,16 @@ export function Card(props) {
 
 			<div className="card-body">
 				<h5 className="card-title">{props.name}</h5>
-				<p className="card-text">
-					Some quick example text to build on the card title and make up the bulk of the card content.
-				</p>
+
+				{/* {Object.entries(store.detailsPeople.properties).map(char => {
+					<p className="card-text" key={char.uid}>
+						{`${property[0]}: ${property[1]}`}
+					</p>;
+				})} */}
 
 				<div className="d-flex justify-content-around">
 					<Link to={`/people/${props.id}`}>
-						<button
-							className="btn btn-outline-primary"
-							onClick={() => actions.getPeople(props.id)}
-							// onClick={() => getCharacterDetails(props.url)}
-						>
+						<button className="btn btn-outline-primary" onClick={() => actions.getPeople(props.id)}>
 							Show more
 						</button>
 					</Link>
