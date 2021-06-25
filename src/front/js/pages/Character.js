f0a6317429b99103f8a722884e7bb6137d8675c2
Jizga <jizga93@gmail.com>
2021-06-25 16:29:41 +0000

    Character's profile is in build

--- src/front/js/pages/Character.js
+++ src/front/js/pages/Character.js
@@ -3,17 +3,20 @@ import { useParams } from "react-router-dom";
 
 import { Context } from "../store/appContext";
 
+import "../../styles/character.scss";
+import clone from "../../img/clone-white.png";
+
 export function Character() {
 	const { uid } = useParams();
-	const { store, actions } = useContext(Context);
+	const { store } = useContext(Context);
 
 	return (
-		<>
+		<div className="container text-white myBox">
 			{store.detailsPeople.properties ? (
-				<div className="container text-white">
+				<div>
 					<div className="row">
 						<div className="col-4">
-							<img src="" />
+							<img src={clone} id="imgCharacter" />
 						</div>
 						<div className="col-8">
 							<h2>{store.detailsPeople.properties.name}</h2>
@@ -22,10 +25,11 @@ export function Character() {
 						</div>
 					</div>
 
-					<hr />
+					<hr className="bg-white" />
 					<div className="row">
 						<p>Detalles del personaje</p>
 						{Object.entries(store.detailsPeople.properties).map(
+							// Drale formato
 							property => `${property[0]}: ${property[1]}`
 						)}
 					</div>
@@ -33,6 +37,6 @@ export function Character() {
 			) : (
 				"Loading..."
 			)}
-		</>
+		</div>
 	);
 }
